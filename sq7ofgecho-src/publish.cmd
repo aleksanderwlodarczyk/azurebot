ec139b1b58ef6ab5ca1d45d26f1c602cff9605f9
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-12-15 19:26:26 +0100

    downloaded basic echo azure source code

---
+++ sq7ofgecho-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=sq7ofgecho-Web-Deploy.pubxml -p:Password=g3acmNgikDjiEAZQCjvC0lvwXLTBSi5tcM3Dxcfi49ngAw8Ae2wc97rY7Ghh
+
